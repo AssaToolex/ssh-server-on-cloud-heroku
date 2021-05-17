c68a31c8ba19826787f97d062ae1dfc5efec5955
Assa Toolex <Assa.Toolex@gmail.com>
2021-05-17 21:28:26 +0300

    Plugin init

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ -z "${USER_IDENTITYS}" ]]; then
+  export USER_IDENTITYS="user01:ssh-rsa ABNaCy2AA...Wi63c8= user01@server01;user02:ssh-rsa BCNy32A54...Zi4535c0= user02@server02;"
+fi
+echo ${USER_IDENTITYS}
+
+
+bash /conf/nginx_tutorial.conf > /etc/nginx/conf.d/tutorial.conf
+echo /etc/nginx/conf.d/tutorial.conf
+cat /etc/nginx/conf.d/tutorial.conf
+
+systemctl start ssh
+
+# rm -rf /etc/nginx/sites-enabled/default
+nginx -g 'daemon off;'
