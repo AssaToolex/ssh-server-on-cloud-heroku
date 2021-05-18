aa0b758a666a43c213f1be227879efc6b5280082
Assa Toolex <Assa.Toolex@gmail.com>
2021-05-18 12:31:38 +0300

    fix ports in sshd and nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,19 +7,19 @@ echo ${USER_IDENTITYS}
 
 
 if [[ -z "${USER_IDENTITYS}" ]]; then
-  export USER_IDENTITYS="user01:ssh-rsa ABNaCy2AA...Wi63c8= user01@server01;;;user02:ssh-rsa BCNy32A54...Zi4535c0= user02@server02"
+  export USER_IDENTITYS="user01:ssh-rsa ABNaCy2AA...Wi63c8= user01@server01;user02:ssh-rsa BCNy32A54...Zi4535c0= user02@server02"
 fi
 echo ${USER_IDENTITYS}
 
 echo "Users:"
-echo ${USER_IDENTITYS} | tr ";;;" "\n"
+echo "${USER_IDENTITYS};" | tr ";" "\n"
 #for user_ident in $(echo ${USER_IDENTITYS} | tr ";" "\n"); do
   # process
   #echo "${user_ident}"
   # .ssh/authorized_keys
 #done
 
-echo "Port ${PORT}" >> /etc/ssh/sshd_config
+echo "Port 22022" >> /etc/ssh/sshd_config
 echo "PermitRootLogin no" >> /etc/ssh/sshd_config
 echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
 # echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
@@ -36,10 +36,10 @@ cat /etc/nginx/sites-available/default
 
 
 # Run OpenSSH server in no-daemon mode
-/usr/sbin/sshd -D
+# /usr/sbin/sshd -D
 
 # Run OpenSSH server in daemon mode
-# /usr/sbin/sshd
+/usr/sbin/sshd
 
 
-# nginx -g 'daemon off;'
+nginx -g 'daemon off;'
