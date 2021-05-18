b85607485e2f3b1e047e1afaa0ed4e3c95cc69b3
Assa Toolex <Assa.Toolex@gmail.com>
2021-05-18 11:42:52 +0300

    add stdout logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-if [[ -z "${USER_IDENTITYS}" ]]; then
-  export USER_IDENTITYS="user01:ssh-rsa ABNaCy2AA...Wi63c8= user01@server01;user02:ssh-rsa BCNy32A54...Zi4535c0= user02@server02;"
+if [[ -z "${SERVER_DOMAIN_NAME}" ]]; then
+  export SERVER_DOMAIN_NAME="xxx-xxx-54345.herokuapp.com"
 fi
 echo ${USER_IDENTITYS}
 
-for user_ident in $(echo ${USER_IDENTITYS} | tr ";" "\n"); do
+
+if [[ -z "${USER_IDENTITYS}" ]]; then
+  export USER_IDENTITYS="user01:ssh-rsa ABNaCy2AA...Wi63c8= user01@server01;;;user02:ssh-rsa BCNy32A54...Zi4535c0= user02@server02"
+fi
+echo ${USER_IDENTITYS}
+
+echo "Users:"
+echo ${USER_IDENTITYS} | tr ";;;" "\n"
+#for user_ident in $(echo ${USER_IDENTITYS} | tr ";" "\n"); do
   # process
-  echo "${user_ident}"
-done
+  #echo "${user_ident}"
+  # .ssh/authorized_keys
+#done
 
 echo "Port 22022" >> /etc/ssh/sshd_config
+echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
 # echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+# echo "AllowUsers Fred Wilma" >> /etc/ssh/sshd_config
 
 
 # bash /conf/nginx_tutorial.conf > /etc/nginx/conf.d/tutorial.conf
