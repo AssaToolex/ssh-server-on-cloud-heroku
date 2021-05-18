ba0334e4c5f3a9299d7282510dc3acda9ef63a98
Assa Toolex <Assa.Toolex@gmail.com>
2021-05-18 21:52:09 +0300

    fixuser & skel

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Sudo fix..."
-ls -la /usr/bin/sudo
-chown root:root /usr/bin/sudo
-chmod u+s /usr/bin/sudo
-ls -la /usr/bin/sudo
+# echo "Sudo fix..."
+# ls -la /usr/bin/sudo
+# chown root:root /usr/bin/sudo
+# chmod u+s /usr/bin/sudo
+# ls -la /usr/bin/sudo
 
-mkdir -p /etc/skel/.ssh
-touch /etc/skel/.ssh/authorized_keys
+# mkdir -p /etc/skel/.ssh
+# touch /etc/skel/.ssh/authorized_keys
