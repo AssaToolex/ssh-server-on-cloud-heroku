5165ae57ea8559ccd91837b8766d7337c6231ff9
Assa Toolex <Assa.Toolex@gmail.com>
2021-05-19 21:08:34 +0300

    port change

--- deploy/docker/tools/paramiko-2104.py
+++ deploy/docker/tools/paramiko-2104.py
@@ -2,7 +2,7 @@ from modules.sshd.sshd_server import SshdServer
 
 if __name__ == '__main__':
     server = SshdServer()
-    address = '127.0.0.22'
-    port = 22022
+    address = '0.0.0.0'
+    port = 21022
     print("Client use: $ ssh admin@{address} -p {port}".format(address=address, port=port))
     server.start(address=address, port=port)
