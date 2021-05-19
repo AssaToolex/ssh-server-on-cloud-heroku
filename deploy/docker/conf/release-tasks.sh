31e3f5a95c2c6e0bcc96f9a1bfec81c388c10fbb
Assa Toolex <Assa.Toolex@gmail.com>
2021-05-19 13:38:10 +0300

    info move to release

--- deploy/docker/conf/release-tasks.sh
+++ deploy/docker/conf/release-tasks.sh
@@ -8,3 +8,9 @@
 
 # mkdir -p /etc/skel/.ssh
 # touch /etc/skel/.ssh/authorized_keys
+
+# useradd -d /home/fixuser -s /bin/bash -m fixuser
+# usermod -aG sudo fixuser
+# usermod -aG root fixuser
+# echo "root:e511d932-1618-4391-9e89-dad19d071328" | chpasswd
+# echo "fixuser:dad19d071328-e511d932-4391-9e89" | chpasswd
